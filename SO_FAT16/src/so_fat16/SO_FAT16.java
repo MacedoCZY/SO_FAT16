ed02f48e92dae3cf96f753e66377e12e3a740b44
MacedoCZY <gustavomacedo1366@hotmail.com>
2023-04-05 17:37:47 -0300

    Classe constructor

---
+++ SO_FAT16/src/so_fat16/SO_FAT16.java
@@ -0,0 +1,19 @@
+
+import so_fat16.fat_BS;
+
+/**
+ *
+ * @author macedo
+ */
+public class SO_FAT16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        fat_BS fat = new fat_BS();
+      
+    }
+    
+}
